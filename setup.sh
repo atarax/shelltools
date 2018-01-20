90d6c30c5f223180e9044a49a454a0155bf92684
atarax <atarax>
2018-01-20 15:32:10 +0100

    setup-script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# install powerline-fonts, needed for agnoster-theme
+sudo apt-get install fonts-powerline
+
 # colors
 RED='\033[0;31m'
 YELLOW='\033[1;33m'
@@ -8,17 +11,16 @@ NO_COLOR='\033[0m'
 # backup old .zshrc if exists
 OLD_ZSHRC=$HOME/.zshrc
 
-echo ${OLD_ZSHRC}
-
 if [ -f $OLD_ZSHRC  ]; then
   DATE=`date '+%Y-%m-%dT%H:%M:%S'`
   BACKUP_ZSHRC="${HOME}/.zshrc-backup-${DATE}"
 
-  echo -e "${YELLOW}Backing up old .zshrc to $BACKUP_ZSHRC${NO_COLOR}"
+  echo -e "${YELLOW}Backing up old .zshrc to $BACKUP_ZSHRC${NO_COLOR}..."
   mv $OLD_ZSHRC $BACKUP_ZSHRC
 fi
- 
 
+echo -e "${YELLOW}Copying .zshrc from repo to ${OLD_ZSHRC}${NO_COLOR}..."
+cp .zshrc ${OLD_ZSHRC}
 
 # install zsh and oh-my-zsh
 sudo apt-get install --yes zsh
