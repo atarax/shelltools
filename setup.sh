80c87e6db40010d1290e45b185e5f8ac91c338f6
atarax <atarax>
2018-01-21 08:38:00 +0100

    setup script done, not as beautiful as expected but works

--- setup.sh
+++ setup.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
 
-# install powerline-fonts, needed for agnoster-theme
-sudo apt-get install fonts-powerline
-
 # colors
 RED='\033[0;31m'
 YELLOW='\033[1;33m'
 NO_COLOR='\033[0m'
 
-# backup old .zshrc if exists
-OLD_ZSHRC=$HOME/.zshrc
+ZSH_RC_LOCATION=$HOME/.zshrc
 
-if [ -f $OLD_ZSHRC  ]; then
-  DATE=`date '+%Y-%m-%dT%H:%M:%S'`
-  BACKUP_ZSHRC="${HOME}/.zshrc-backup-${DATE}"
+# install things, powerline-fonts, needed for agnoster-theme,
+# zsh, and curl to be able to install 
+sudo apt-get install --yes fonts-powerline zsh curl 
 
-  echo -e "${YELLOW}Backing up old .zshrc to $BACKUP_ZSHRC${NO_COLOR}..."
-  mv $OLD_ZSHRC $BACKUP_ZSHRC
+if [ ! -n "$ZSH" ]; then
+  ZSH=~/.oh-my-zsh
+fi
+if [ ! -d "$ZSH" ]; then
+  printf "${RED}oh-my-zsh is not installed, exit zsh after it's installation!${NO_COLOR}\n"
+  sleep 5
 fi
 
-echo -e "${YELLOW}Copying .zshrc from repo to ${OLD_ZSHRC}${NO_COLOR}..."
-cp .zshrc ${OLD_ZSHRC}
-
-# install zsh and oh-my-zsh
-sudo apt-get install --yes zsh
+# install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+printf "${YELLOW}Copying .zshrc from repo to ${OLD_ZSHRC}${NO_COLOR}...\n"
+cp .zshrc ${ZSH_RC_LOCATION}
 
+printf "${YELLOW}Please log out and in again for changes to take effect!${OLD_ZSHRC}${NO_COLOR}...\n"
